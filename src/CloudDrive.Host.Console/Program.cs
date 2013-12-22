ef5b6f5a70f389fb1880ec0c6ab65958c6afb165
James Layton <james.layton@vortx.com>
2013-12-21 17:02:05 -0800

    SkyDrive inital work and nuget config

--- src/CloudDrive.Host.Console/Program.cs
+++ src/CloudDrive.Host.Console/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SkyNet;
+using SkyNet.Client;
+using CloudDrive.Service.SkyDrive;
 
 namespace CloudDrive.Host.Console
 {
 	class Program
 	{
+		static Client SkyDriveClient;
+		static PersistantAccessToken AccessToken;
+
 		static void Main(string[] args)
 		{
+			CreateSkyClient();
+
+			var contents = SkyDriveClient.GetContents(string.Empty);
+		}
+
+		static void CreateSkyClient()
+		{
+			AccessToken = new PersistantAccessToken(ConfigurationManager.AppSettings["CloudDrive.Service.SkyDrive.ConfigurationFolder"]);
+			SkyDriveClient = new Client(Settings.ClientId, Settings.ClientSecret, Settings.LiveLoginCallbackUrl,
+				AccessToken.AccessToken != null ? AccessToken.AccessToken.Access_Token : null,
+				AccessToken.AccessToken != null ? AccessToken.AccessToken.Refresh_Token : null);
+
+			if (AccessToken.AccessToken != null)
+			{
+				AccessToken.AccessToken = SkyDriveClient.RefreshAccessToken();
+			}
 		}
 	}
 }
